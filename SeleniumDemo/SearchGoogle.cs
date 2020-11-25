ad402b8b7e634e325ecaa8d41976dd532d518403
Anurag Patidar <anuragpatidar@yahoo.in>
2020-11-25 23:43:10 +0530

    Update SearchGoogle.cs

--- SeleniumDemo/SearchGoogle.cs
+++ SeleniumDemo/SearchGoogle.cs
@@ -45,36 +45,7 @@ namespace SeleniumDemo
 
         }
 
-        [TestMethod]
-        public void TestEnd()
-        {
-            var driver = new ChromeDriver();
-
-            {
-                //Notice navigation is slightly different than the Java version
-                //This is because 'get' is a keyword in C#
-                driver.Navigate().GoToUrl("http://www.google.com/");
-
-                // Find the text input element by its name
-                IWebElement query = driver.FindElement(By.Name("q"));
-
-                // Enter something to search for
-                query.SendKeys("Anurag");
-
-                // Now submit the form. WebDriver will find the form for us from the element
-                query.Submit();
-
-                // Google's search is rendered dynamically with JavaScript.
-                // Wait for the page to load, timeout after 10 seconds
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(d => d.Title.StartsWith("Anurag", StringComparison.OrdinalIgnoreCase));
-
-                // Should see: "Cheese - Google Search" (for an English locale)
-                Assert.AreEqual(driver.Title, "Anurag - Google Search");
-
-                driver.Quit();
-            }
-        }
+        
 
     }
 }
